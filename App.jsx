9190b4f34751270095ea79990c7b7b360bdd95b0
Pawan41 <83387085+Pawan41@users.noreply.github.com>
2021-06-05 05:02:04 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+
+function App(){
+  return(
+<div>
+  <Header />
+  <Footer/>
+  <Note/>
+  <Note/>
+  <Note/>
+</div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>This was an amazing Bootcamp taked by Shaurya shina .We 
+        covered everything from scratch includingjavascript
+        and Node.js,HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
